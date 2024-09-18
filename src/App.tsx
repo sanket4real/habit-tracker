4996e0a4c8f49bbad853aa2c65769f9a90dd26bd
Sanket <desaisanket68@gmail.com>
2024-09-18 18:15:04 +0530

    added form for habits

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { Container, Typography } from "@mui/material";
+import AddHabitForm from "./components/add-habit-form";
+
 function App() {
 	return (
-		<>
-			<h1>habit Tracker</h1>
-		</>
+		<Provider store={store}>
+			<Container maxWidth="md">
+				<Typography component="h1" variant="h2" align="center">
+					Habit Tracker
+				</Typography>
+				<AddHabitForm />
+			</Container>
+		</Provider>
 	);
 }
 
